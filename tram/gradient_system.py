8449855ad2e0d91ffe3bb28a2ce23a8ca05524c3
Andreas Bittracher <bittracher@mi.fu-berlin.de>
2019-07-11 15:30:11 +0200

    added doublewell-demo

--- tram/gradient_system.py
+++ tram/gradient_system.py
@@ -148,18 +148,18 @@ class GradientSystem(object):
         var = (dt*2/self.beta)*np.eye(sysdim)
 
         # pre-draw the Brownian motion
-        dW = np.random.multivariate_normal(mean, var, nsteps*npoints).reshape((npoints,sysdim,-1))
+        #dW = np.random.multivariate_normal(mean, var, nsteps*npoints).reshape((npoints,sysdim,-1))
 
         print("Burst integration...")
         for i in bar(range(nsteps)):
             nablaV = self.gradPot(r0)
 
             # draw Brownian motion
-            # dW = np.random.multivariate_normal(mean, var, npoints)
+            dW = np.random.multivariate_normal(mean, var, npoints)
 
             # update position
-            #r0 = r0 - dt*nablaV + dW
-            r0 = r0 - dt*nablaV + dW[:, :, i]
+            r0 = r0 - dt*nablaV + dW
+            #r0 = r0 - dt*nablaV + dW[:, :, i]
         return r0
 
     def generateTestpoints(self, n, dist='uniform'):
